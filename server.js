d9a983674563c66b959a06dd2967e001e6689d4d
Jason L <jkleitz27@gmail.com>
2022-06-19 14:02:15 -0400

    update for heroku to see if that doesn't crash the app

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3001;
 const app = express(); 
 
 
-//to do: get routes working
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// //to do: get routes working
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 
-// Use apiRoutes
-app.use('/api/notes', apiRoutes);
-app.use('/', htmlRoutes);
+// // Use apiRoutes
+// app.use('/api/notes', apiRoutes);
+// app.use('/', htmlRoutes);
 
 
 
@@ -29,83 +29,83 @@ app.use(express.static('public'));
 
 
 
-// // request data from notes
-// const { notes } = require('./db/db.json');
+// request data from notes
+const { notes } = require('./db/db.json');
 
-// // function taking the data from req.body 
-// function createNote (body, notesArray) {
-//     const note = body; 
-//     notesArray.push(note); 
+// function taking the data from req.body 
+function createNote (body, notesArray) {
+    const note = body; 
+    notesArray.push(note); 
 
 
-//     fs.writeFileSync(
-//         path.join(__dirname, './db/db.json'),
-//         JSON.stringify({ notes : notesArray }, null, 2)
-//     );
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes : notesArray }, null, 2)
+    );
     
-//     return note; 
-// };
+    return note; 
+};
 
-// // validating data
-// function noteValidate (note) {
-//     if (!note.title || typeof note.title !== 'string') {
-//         return false; 
-//     }
-//     if (!note.text || typeof note.text !== "string") {
-//         return false;
-//     }
-//     return true;   
-// };
+// validating data
+function noteValidate (note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false; 
+    }
+    if (!note.text || typeof note.text !== "string") {
+        return false;
+    }
+    return true;   
+};
 
-// //API routes, gets, posts, and deletes 
+//API routes, gets, posts, and deletes 
 
-// // route GET 
-// app.get('/api/notes', (req, res) => {
-//     res.json(notes); 
-// });
+// route GET 
+app.get('/api/notes', (req, res) => {
+    res.json(notes); 
+});
 
 
-// app.post('/api/notes', (req, res) => {
-//     // set id 
-//     req.body.id = notes.length.toString(); 
+app.post('/api/notes', (req, res) => {
+    // set id 
+    req.body.id = notes.length.toString(); 
 
     
-//     if (!noteValidate(req.body)) {
-//         res.status(400).send('Please format this note.'); 
+    if (!noteValidate(req.body)) {
+        res.status(400).send('Please format this note.'); 
     
-//     } else {
+    } else {
     
-//         const note = createNote(req.body, notes); 
+        const note = createNote(req.body, notes); 
 
-//         res.json(note);
-//     }
-// });
+        res.json(note);
+    }
+});
 
-// // delete notes
-// app.delete('/api/notes/:id', (req, res) => {
-//     const id = req.params.id;
-//     let note;
+// delete notes
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note;
 
-//     notes.map((element, index) => {
-//         if (element.id == id){
-//         note = element
-//         notes.splice(index, 1)
-//         return res.json(note);
-//         } 
+    notes.map((element, index) => {
+        if (element.id == id){
+        note = element
+        notes.splice(index, 1)
+        return res.json(note);
+        } 
     
-//     })
-// });
+    })
+});
 
-// //html routes
-// // route to index.html 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname,'./public/index.html'));
-// }); 
+//html routes
+// route to index.html 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+}); 
 
-// // route to notes.html 
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname,'./public/notes.html'));
-// }); 
+// route to notes.html 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+}); 
 
 
 
